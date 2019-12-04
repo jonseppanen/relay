098a7c2ce3d06d51ed8c097a53e2c7f02badb078
jonseppanen <ashtefere@gmail.com>
2019-12-04 12:41:25 +1000

    Refactor... or... or... or...

--- index.js
+++ index.js
@@ -1,67 +1,36 @@
 class Spark {
-    constructor(node, relay) {
-        this.node = node;
+    constructor(nodes, relay) {
+        this.nodes = nodes;
         this.relay = relay;
-        this.template = (node.outerHTML || node.nodeValue).slice(0);
-        this.watched = [];
+
+        if (nodes[0].classList && nodes[0].classList.contains('RelayTarget')) {
+            this.dotPath = nodes[0].getAttribute('data-value');
+        }
+        else {
+            this.template = nodes.map(node => (node.outerHTML || node.nodeValue)).join();
+        }
+        this.watched = this.dotPath ? [this.dotPath] : [];
         this.update();
     }
 
-    bigote = /\{\{([^\{\{\}\}]+)\}\}/g;
-
-    usurp = (replaced, replacer) => {
-        replaced.parentNode.insertBefore(replacer, replaced.nextSibling);
-        replaced.parentNode.removeChild(replaced)
-        return replacer;
-    }
-
     update = () => {
-
-        let domfrag = document.createDocumentFragment();
+        this.watched = this.dotPath ? [this.dotPath] : [];
         let workNode = document.createElement('div');
-        domfrag.appendChild(workNode);
-        let newTemplate = this.template.slice(0).replace(this.bigote, (match, variable) => {
+        workNode.innerHTML = this.relay.inject((this.template || (this.dotPath.split('.').reduce((o, i) => o && o[i], this.relay.data) || '')).slice(0), this.watched, this.relay);
+        workNode.querySelectorAll('.RelayTarget').forEach(newSpark => this.relay.register([newSpark]))
 
-
-            let value = variable.split('.').reduce((o, i) => o && o[i], this.relay.rootProxy) || '';
-            if (!this.watched.includes(variable)) this.watched.push(variable);
-
-            if (value.includes("{{")) {
-                let templateWorker = document.createElement("div");
-                templateWorker.innerHTML = value;
-                let newString = '';
-
-                templateWorker.childNodes.forEach(child => {
-                    let valCheck = (child.nodeValue || child.outerHTML).slice(0);
-                    newString += (valCheck.includes("{{")) ? '<span class="RelayTarget">' + valCheck + '</span>' : valCheck
-                })
-
-                templateWorker.remove();
-                return newString;
-            }
-            else {
-                return value;
-            }
-        });
-
-        workNode.innerHTML = newTemplate;
-        let newNode = workNode.firstChild;
-
-        if (newNode.nodeType !== 3) {
-            newNode.querySelectorAll('.RelayTarget').forEach(newSpark => {
-                this.relay.register(this.usurp(newSpark, newSpark.firstChild))
-            })
-        }
-
-        this.node = this.usurp(this.node, newNode)
+        let oldNodes = this.nodes;
+        let newNodes = Array.from(workNode.childNodes).reverse();
+        newNodes.forEach(node => oldNodes[0].parentNode.insertBefore(node, oldNodes[0].nextSibling));
+        this.nodes = newNodes;
+        oldNodes.forEach(node => node.remove())
     }
 }
-
 class Relay {
-    register = (el) => {
-        const sNode = new Spark(el, this);
+    register = (nodes) => {
+        const sNode = new Spark(nodes, this);
         this.registry.push(sNode);
-        return sNode.node;
+        return sNode.nodes;
     }
 
     getDotPath = (target, property) => {
@@ -76,16 +45,30 @@ class Relay {
         return pathString
     }
 
-    updateWatchers = (target, property) => {
-        this.registry.filter(sNode => sNode.watched.includes(this.getDotPath(target, property))).forEach(sNode => sNode.update());
-        this.registry = this.registry.filter(sNode => document.getElementById("Relay").contains(sNode.node));
-        //   console.log(this.registry);
+    inject = (template, watcher) => {
+        const bigote = /\{\{([^\{\{\}\}]+)\}\}/g;
+        return template.slice(0).replace(bigote, (match, variable) => {
+            let value = variable.split('.').reduce((o, i) => o && o[i], this.data) || '';
+
+            if (!value) {
+                if (!watcher.includes(variable)) watcher.push(variable); return '';
+            }
+
+            if (!value.includes("{{") && !value.includes("<")) {
+                if (!watcher.includes(variable)) watcher.push(variable);
+                return value
+            }
+
+            return '<script class="RelayTarget" data-value="' + variable + '" style="display:none;"></script>';
+        });
     }
 
     relayProxy = {
         set: (target, property, value) => {
+            if (JSON.stringify(target[property]) === JSON.stringify(value)) return false;
             target[property] = (Array.isArray(value) || typeof value === "object") ? () => { this.ancestry.set(value, { parent: target, keyname: property }); return new Proxy(value, this.relayProxy) } : value;
-            this.updateWatchers(target, property);
+            this.registry.filter(sNode => sNode.watched.includes(this.getDotPath(target, property))).forEach(sNode => sNode.update());
+            this.registry = this.registry.filter(sNode => this.rootElement.contains(sNode.nodes[0]));
             return true;
         }
     };
@@ -93,13 +76,19 @@ class Relay {
     constructor(rootElement) {
         this.ancestry = new WeakMap();
         this.rootElement = rootElement;
-        this.registry = [new Spark(this.rootElement, this)];
-        this.rootProxy = new Proxy({}, this.relayProxy);
-        return this.rootProxy;
+        this.registry = [];
+        this.data = new Proxy({}, this.relayProxy);
+
+        rootElement.childNodes.forEach(node => {
+            let nodeContent = (node.outerHTML || node.nodeValue).slice(0)
+            if (nodeContent.includes('{{')) {
+                this.register([node])
+            }
+        })
+
+        return this.data;
     }
 }
-
-
 
 const R = new Relay(document.getElementById("Relay"))
 
@@ -109,11 +98,13 @@ R.anchor1 = 'prestring<div>anchor1{{deepclass}}</div>'
 R.anchor2 = 'anchor2<div>{{deep.shit}}</div>'
 R.anchor3 = '<div><h2>anchor3{{deepclass}}</h2></div>'
 R.anchor4 = '<div>anchor4{{deepclass}}</div>'
-R.anchor5 = '<div>anchor5</div>'
+R.anchor5 = '<div>anchor5{{newdeeptest}}</div>'
 R.anchor6 = '<div>anchor6</div>'
 R.anchor7 = '<div>anchor7{{deep.shit}}</div>'
 R.deepclass = 'fucky'
 R.class1 = 'notfucky'
+R.newdeeptest = '<div>cascade{{newdeeptest2}}</div>'
+R.newdeeptest2 = ' to here'
 
 const gg = () => {
     R.deepclass = 'hnggggggg'
